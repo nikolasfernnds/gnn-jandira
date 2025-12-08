7c627a1aaa3450607eab2a4691a41f9811931e6a
Gabryel Fillipe <byelfcsilva@gmail.com>
2025-12-08 18:47:59 -0300

    feat(front): Desenvolvendo funções para deixar a tela de ocorrencias dinamica

---
+++ frontend/assets/js/ocorrencias.js
@@ -0,0 +1,33 @@
+'use strict'
+
+export async function listarOcorrencias() {
+    const url = `https://gnn-jandira.onrender.com/v1/gnn/ocorrencia`
+    const response = await fetch(url)
+    
+    if (!response.ok) throw new Error(`Erro na API: ${response.status}`) // Verifica se deu 404 ou 500
+    return response.json()
+}
+
+export async function listarOcorrenciaPeloId(id) {
+    const url = `https://gnn-jandira.onrender.com/v1/gnn/ocorrencia/${id}`
+    const response = await fetch(url)
+
+    if (!response.ok) throw new Error(`Erro na API: ${response.status}`)
+    return response.json()
+}
+
+export async function listarOcorrenciasDoUsuario(usuarioId) {
+    const url = `https://gnn-jandira.onrender.com/v1/gnn/ocorrencia/usuario/${usuarioId}`
+    const response = await fetch(url)
+
+    if (!response.ok) throw new Error(`Erro na API: ${response.status}`)
+    return response.json()
+}
+
+export async function listarOcorrenciaPeloTipo(tipoOcorrencia) {
+    const url = `https://gnn-jandira.onrender.com/v1/gnn/ocorrencia/tipo/${tipoOcorrencia}`
+    const response = await fetch(url)
+
+    if (!response.ok) throw new Error(`Erro na API: ${response.status}`)
+    return response.json()
+}
